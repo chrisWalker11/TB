eb0cf512cfa24e38db74233ab66cb6e00db5b493
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2020-08-21 09:05:45 -0400

    Update tinderBot.py

--- tinderBot.py
+++ tinderBot.py
@@ -2,30 +2,38 @@ import time
 import pyautogui
 import random
 value = random.randint(0, 1000)
+
+px = int(input("input x coordinate for next photo: "))
+py = int(input("input y coordinate for next photo: "))
+lx = int(input("input x coordinate for like: "))
+ly = int(input("input y coordinate for like: "))
+dx = int(input("input x coordinate for dislike: "))
+dy = int(input("input y coordinate for dislike: "))
+
+
 while (1 == 1):
     if value % 2 == 0:
-       
+
         time.sleep(random.randint(10, 100))
-        pyautogui.click() #click through photo
+        pyautogui.click(px, py) #click through photo
         
         time.sleep(random.randint(10, 100))
-        pyautogui.click() #click through photo
+        pyautogui.click(px, py) #click through photo
         
         time.sleep(random.randint(10, 100))
-        pyautogui.click() #click through photo
+        pyautogui.click(px, py) #click through photo
         
-        time.sleep(random.randint(5, 500))
-        pyautogui.click() #click like
+        time.sleep(random.randint(5, 90))
+        pyautogui.click(lx, ly) #click like
     else:
-        
-        time.sleep(random.randint(5,45))
-        pyautogui.click() #photo
-        
+
         time.sleep(random.randint(10, 70))
-        pyautogui.click() #photo
+        pyautogui.click(px, py) #photo
 
         time.sleep(random.randint(10,70))
-        pyautogui.click() #photo
+        pyautogui.click(px, py) #photo
 
-        time.sleep(random.randint(1, 80))
-        pyautogui.click() #dislike
+        time.sleep(random.randint(5, 80))
+        pyautogui.click(dx, dy) #dislike
+
+
