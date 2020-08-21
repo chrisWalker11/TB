9bd34b6fdd8fad7f3b5344cef4c90f81d576643f
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2020-08-21 07:36:05 -0400

    Update tinderBot.py

--- tinderBot.py
+++ tinderBot.py
@@ -4,12 +4,7 @@ import random
 value = random.randint(0, 1000)
 while (1 == 1):
     if value % 2 == 0:
-        time.sleep(random.randint(10, 100))
-        pyautogui.click() #click on bio
-        
-        time.sleep(random.randint(30, 400))
-        pyautogui.scroll(10) #scroll down bio
-
+       
         time.sleep(random.randint(10, 100))
         pyautogui.click() #click through photo
         
@@ -22,12 +17,10 @@ while (1 == 1):
         time.sleep(random.randint(5, 500))
         pyautogui.click() #click like
     else:
-        time.sleep(random.randint(10, 80))
-        pyautogui.click() #click bio
-
-        time.sleep(random.randint(30, 200))
-        pyautogui.scroll() #scroll down for the bio
-
+        
+        time.sleep(random.randint(5,45))
+        pyautogui.click() #photo
+        
         time.sleep(random.randint(10, 70))
         pyautogui.click() #photo
 
